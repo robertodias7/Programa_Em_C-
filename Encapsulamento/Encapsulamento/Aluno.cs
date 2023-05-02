9204ccbebf499e444529fae3f1eed833ab41d2a2
robertodias7 <dias.roberto.16@gmail.com>
2023-05-02 11:10:23 -0300

    Exercício de Encapsulamento

---
+++ Encapsulamento/Encapsulamento/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    internal class Aluno
+    {
+        private double nota1, nota2;
+
+        private double media()
+        {
+            return (nota1 + nota2) /2;
+        }
+
+        public void mensagem()
+        {
+            Console.Write("Digite a Nota 1: ");
+            nota1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite a Nota 2: ");
+            nota2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Sua média é: " + media());
+        }
+    }
+}
