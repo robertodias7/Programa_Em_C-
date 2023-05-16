62ba38674bb91c9b934105089a945a40a9323d37
robertodias7 <dias.roberto.16@gmail.com>
2023-05-16 18:51:30 -0300

    Exercício de POO

---
+++ PooExe/PooExe/CadAluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooExe
+{
+    internal class CadAluno
+    {
+        public string NOME;
+        public int SALA;
+        public float[] NOTA = new float[4];
+
+        public float CalMedia()
+        {
+            byte i;
+            float SOMA = 0, MEDIA;
+            
+            for(i = 0; i < 3; i++)
+                SOMA += NOTA[i];
+            MEDIA = SOMA / 4;
+            return MEDIA;
+            
+        }
+
+    }
+}
---
+++ PooExe/PooExe/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooExe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CadAluno ALUNO = new CadAluno();
+
+            Console.Write("Entre o nome ..: ");
+            ALUNO.NOME = Console.ReadLine();
+            Console.Write("Entre a sala ...:");
+            ALUNO.SALA = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Entre as notas:");
+            Console.WriteLine();
+            for (int i = 0; i<=3; i++)
+            {
+                Console.Write("{0}a. nota ......: ", i + 1);
+                ALUNO.NOTA[i] = float.Parse(Console.ReadLine());
+            }
+            ALUNO.CalMedia();
+            Console.WriteLine();
+            Console.WriteLine($"Nome ..........: {ALUNO.NOME}");
+            Console.WriteLine();
+            Console.WriteLine("Notas:");
+            Console.WriteLine();
+
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.Write($"{i + 1}a. nota ......: ");
+                Console.WriteLine("{0,5:0.00}", ALUNO.NOTA[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Média .........: {0,5:0.00}", ALUNO.CalMedia());
+
+            Console.WriteLine();
+            Console.WriteLine("Tecle algo para encerrar... ");
+            Console.ReadKey();
+            
+        }
+    }
+}
