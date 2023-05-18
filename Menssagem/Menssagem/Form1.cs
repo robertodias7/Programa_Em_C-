3c8132cd3813114ef0cae5df7e837e6d479ed213
robertodias7 <dias.roberto.16@gmail.com>
2023-05-18 16:51:41 -0300

    Exercícios de MessageBox

---
+++ Menssagem/Menssagem/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menssagem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult  RESP;
+            RESP = MessageBox.Show
+            (
+                "Você é honesto?",
+                "Uma pergunta",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+             );
+            if (RESP == DialogResult.Yes)
+                MessageBox.Show
+                (
+                    "Parabéns!!!",
+                   "Respondeu SIM",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Information
+                 );
+            else
+                MessageBox.Show
+                (
+                    "Seu Pilantra!!!",
+                    "Respondeu Não",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                 );
+        }
+    }
+}
---
+++ Message/Message/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Message
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*MessageBox.Show
+            (
+                "Mensagem",
+                "Título",
+                MessageBoxButtons.OKCancel
+
+            OK  CANCELAR
+
+             );*/
+
+            /*MessageBox.Show
+            (
+                "Mensagem",
+                "Título",
+                MessageBoxButtons.AbortRetryIgnore
+
+                ANULAR REPETIR IGNORAR
+
+             );*/
+
+            /*MessageBox.Show
+            (
+                "Mensagem",
+                "Título",
+                MessageBoxButtons.YesNoCancel
+
+                SIM NÃO CANCELAR
+
+             );*/
+
+            /*MessageBox.Show
+            (
+                "Mensagem",
+                "Título",
+                MessageBoxButtons.YesNo
+
+                SIM NÃO
+             );*/
+
+            /* MessageBox.Show
+             (
+                 "Mensagem",
+                 "Título",
+                 MessageBoxButtons.RetryCancel
+
+                 REPETIR CANCELAR
+
+              );*/
+
+            /* MessageBox.Show
+             (
+                 "Mensagem",
+                 "Título",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Error
+
+                 Mensagem de ERRO
+                 OK
+
+              );*/
+
+            /* MessageBox.Show
+             (
+                 "Mensagem",
+                 "Título",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Question
+
+              //Mensagem de Pergunta
+              //OK
+
+              );*/
+
+            /*MessageBox.Show
+            (
+                "Mensagem",
+                "Título",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning
+
+             //Mensagem de Alerta
+             //OK
+
+             );*/
+
+            MessageBox.Show
+            (
+                "Mensagem",
+                "Título",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+
+             //Mensagem de Informação
+             //OK
+
+             );
+
+            MessageBox.Show
+            (
+                "Mensagem",
+                "Título",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button3,
+                MessageBoxOptions.RightAlign
+
+                //Mensagem de ERRO
+                //OK
+
+             );
+        }
+    }
+}
