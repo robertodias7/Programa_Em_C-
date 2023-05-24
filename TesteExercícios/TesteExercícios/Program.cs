fdbfc77947cb084beba13390c16895003dca3c14
robertodias7 <dias.roberto.16@gmail.com>
2023-05-23 21:28:20 -0300

    Exercícios novos

--- TesteExercícios/TesteExercícios/Program.cs
+++ TesteExercícios/TesteExercícios/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
@@ -11,27 +13,129 @@ namespace TesteExercícios
 
     internal class Program
     {
-        static void Main(string[] args)
+
+        
+
+        public static void Main(string[] args)
         {
-            int[] A = new int[10];
-            int[] B = new int[10];
+            Menu();
+        }
+        public static void Menu()
+        {
+            Console.Title = "Calculadora";
+            Console.Clear();
+            Console.WriteLine("-------CALCULADORA-------");
+            Console.WriteLine();
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("[1] - Soma");
+            Console.WriteLine("[2] - Subtração");
+            Console.WriteLine("[3] - Multiplicação");
+            Console.WriteLine("[4] - Divisão");
+            Console.WriteLine("[0] - Sair");
+            Console.WriteLine();
+            Console.Write("Sua Opção ...: ");
+            
 
-            for (int i = 0; i <= 9; i++)
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res)
             {
-                A[i] = int.Parse(Console.ReadLine());
-                Console.WriteLine("Média do aluno{0}", A[i]);
+                case 0: Environment.Exit(0); break;
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Multiplicacao(); break;
+                case 4: Divisao(); break;
+                default: Menu(); break;
+            }
+            Console.ReadKey();
+        }
+        public static void Soma()
+        {   
+            double resultado, num1, num2;
 
-                if (A[i] % 2 == 0)
-                {
-                    B[i] = A[i];
-                    B[i] *= B[i];
-                }
-                else
-                {
-                    B[i] = A[i];
-                    B[i] += B[i];
-                }
-            }        
+            Console.Clear();
+            Console.WriteLine("SOMA");
+            Console.WriteLine();
+            Console.Write("Entre com o primeiro número .: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Entre com o segundo número ..: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            resultado = num1 + num2;
+
+            Console.WriteLine();
+            Console.WriteLine($"O Resultado da operação é: {resultado}");
+            Console.ReadKey();
+            Menu();
+
+        }
+
+        public static void Subtracao()
+        {
+            double resultado, num1, num2;
+
+            Console.Clear();
+            Console.WriteLine("SUBTRAÇÃO");
+            Console.WriteLine();
+            Console.Write("Entre com o primeiro número .: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Entre com o segundo número ..: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            resultado = num1 - num2;
+
+            Console.WriteLine();
+            Console.WriteLine($"O Resultado da operação é: {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        public static void Multiplicacao()
+        {
+            double resultado, num1, num2;
+
+            Console.Clear();
+            Console.WriteLine("MULTIPLICAÇÃO");
+            Console.WriteLine();
+            Console.Write("Entre com o primeiro número .: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Entre com o segundo número ..: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            resultado = num1 * num2;
+
+            Console.WriteLine();
+            Console.WriteLine($"O Resultado da operação é: {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        public static void Divisao()
+        {
+            double resultado, num1, num2;
+
+            Console.Clear();
+            Console.WriteLine("DIVISÃO");
+            Console.WriteLine();
+            Console.Write("Entre com o primeiro número .: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Entre com o segundo número ..: ");
+            num2 = double.Parse(Console.ReadLine());
+
+           
+            resultado = num1  / num2;
+            if(num2 == 0)
+            {
+                Console.WriteLine("ERRO! Divisão por Zero");
+                Console.ReadKey ();
+                Menu();
+            }
+            else
+            Console.WriteLine();
+            Console.WriteLine($"O Resultado da operação é: {resultado}");
+            Console.ReadKey();
+            Menu();
         }
     }
+   
 }
