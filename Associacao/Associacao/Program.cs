c8a3cb0b6ef1022a9f7a57bea4edfabcc214874c
robertodias7 <dias.roberto.16@gmail.com>
2023-06-01 17:07:05 -0300

    Orientação a objetos - UNIP

---
+++ Associacao/Associacao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Associacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa cliente = new Pessoa();
+            cliente.Nome = "Roberto";
+
+            Endereco casa = new Endereco();
+            casa.Logradouro = "Q 41 lt 11 JdA";
+            casa.Numero = 1;
+            casa.Cidade = "SAD";
+
+            cliente.Residencia = casa;
+            Console.WriteLine(cliente.Residencia);
+            Console.ReadKey();
+        }
+    }
+}
